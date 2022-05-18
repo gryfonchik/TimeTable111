c221bc9afa95589b98f118e798c8a21fde452b3e
ada0l <andreika.varfolomeev@yandex.ru>
2022-05-18 20:33:24 +1000

    add relationships for schedule item

--- backend/schedule_item/models.py
+++ backend/schedule_item/models.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 
 from backend.bell.models import Bell
 from backend.classroom.models import Classroom
@@ -15,10 +16,18 @@ class ScheduleItem(Base):
 
     id = Column(Integer, primary_key=True)
     course_id = Column(Integer, ForeignKey(Course.id), nullable=False)
+    course = relationship("Course")
     teacher_id = Column(Integer, ForeignKey(Teacher.id), nullable=False)
+    teacher = relationship("Teacher")
     classroom_id = Column(Integer, ForeignKey(Classroom.id), nullable=False)
+    classroom = relationship("Classroom")
     bell_id = Column(Integer, ForeignKey(Bell.id), nullable=False)
+    bell = relationship("Bell")
     week_id = Column(Integer, ForeignKey(Week.id), nullable=False)
+    week = relationship("Week")
     group_id = Column(Integer, ForeignKey(Group.id), nullable=True)
+    group = relationship("Group")
     subgroup_id = Column(Integer, ForeignKey(Subgroup.id), nullable=True)
+    subgroup = relationship("Subgroup")
     type_schedule_item_id = Column(Integer, ForeignKey(TypeScheduleItem.id), nullable=True)
+    type_schedule_item= relationship("TypeScheduleItem")
