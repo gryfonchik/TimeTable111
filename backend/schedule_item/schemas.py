d0a8961b83a8142a32fc802b82bf9cf5d5217f99
ada0l <andreika.varfolomeev@yandex.ru>
2022-05-18 20:32:18 +1000

    schedule item schemas

--- backend/schedule_item/schemas.py
+++ backend/schedule_item/schemas.py
@@ -1,12 +1,28 @@
 from pydantic import BaseModel
 
+from backend.bell.schemas import BellPydantic
+from backend.classroom.schemas import ClassroomPydantic
+from backend.course.schemas import CoursePydantic
+from backend.group.schemas import GroupPydantic, SubgroupPydantic
+from backend.teacher.schemas import TeacherPydantic
+from backend.type_schedule_item.schemas import TypeScheduleItemPydantic
+from backend.week.schemas import WeekPydantic
+
 
 class ScheduleItemBasePydantic(BaseModel):
-    label: str
+    pass
 
 
 class ScheduleItemPydantic(ScheduleItemBasePydantic):
     id: int
+    course: CoursePydantic
+    teacher: TeacherPydantic
+    bell: BellPydantic
+    group: GroupPydantic | None
+    subgroup: SubgroupPydantic | None
+    type_schedule_item: TypeScheduleItemPydantic
+    classroom: ClassroomPydantic
+    week: WeekPydantic
 
     class Config:
         orm_mode = True
