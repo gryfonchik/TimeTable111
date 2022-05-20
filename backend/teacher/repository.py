6eba2eb6c62d7fca714e1b7e7405dd15644c2a89
ada0l <andreika.varfolomeev@yandex.ru>
2022-05-20 14:52:02 +1000

    teacher filter by course

--- backend/teacher/repository.py
+++ backend/teacher/repository.py
@@ -1,7 +1,11 @@
 from typing import Type
 
+from sqlalchemy import select
+
 from backend.core.repository import BaseRepository
+from backend.course_teacher.models import course_teacher_table
 from backend.teacher import schemas, models
+from backend.teacher.schemas import TeacherFilterPydantic
 
 
 class TeacherRepository(
@@ -16,3 +20,12 @@ class TeacherRepository(
     @property
     def _model(self) -> Type[models.Teacher]:
         return models.Teacher
+
+    async def get_by_course(self, filter_in: TeacherFilterPydantic):
+        q = select(self._model)
+        if filter_in.course_id:
+            q = q. \
+                join(course_teacher_table). \
+                filter(course_teacher_table.columns.course_id.in_([filter_in.course_id]))
+        objs = await self.session.execute(q)
+        return objs.scalars().all()
